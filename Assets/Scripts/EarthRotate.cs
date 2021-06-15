11d09305c4adbf1aba2b47c07cba61e4adc5c3cd
hpruthi1 <46549782+hpruthi1@users.noreply.github.com>
2021-06-16 03:32:18 +0530

    Code Clean-up

---
+++ Assets/Scripts/EarthRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EarthRotate : MonoBehaviour
+{
+    public float RotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate((Vector3.up * RotationSpeed) * Time.deltaTime);
+    }
+}
